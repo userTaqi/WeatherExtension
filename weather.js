7f2d86fafb8583da56251ea4c55b0f78b35dd54f
Taqi Hasan <92113141+userTaqi@users.noreply.github.com>
2023-04-02 07:11:00 +0100

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,30 @@
+document.querySelector("[data-btn]").addEventListener("click", fun);
+
+function fun() {
+  // Set the city and API key variables
+  const city = document.querySelector("[data-city]").value;
+  const key = "ca24585347bf4e708d812731230204";
+
+  // Construct the API URL with the city and API key variables
+  const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`;
+  // Use the fetch() method to retrieve the data from the API
+
+  fetch(url)
+    .then((response) => response.json()) // Convert the response to JSON format
+    .then((data) => {
+      // Extract the temperature in Celsius from the object in the data
+      const temp_c = data.current.temp_c;
+      const temp_f = data.current.temp_f;
+      const imageSource = data.current.condition.icon;
+      const location = data.location.country + ", " + data.location.name;
+      const localTime = data.location.localtime;
+
+      // Update the text content of the elements with the values
+      document.querySelector("[data-img]").setAttribute("src", imageSource);
+      document.querySelector("[data-c]").textContent = temp_c + "°C";
+      document.querySelector("[data-f]").textContent = temp_f + "°F";
+      document.querySelector("[data-location]").textContent = location;
+      document.querySelector("[data-localtime]").textContent = localTime;
+    })
+    .catch((error) => console.log(error)); // Log any errors that occur during the process
+}
